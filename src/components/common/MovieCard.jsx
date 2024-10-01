f24040d214d7ddb8bb3a78fa16cf4ad4b85c8be9
Sangeeta <sangeetaswainlucky@gmail.com>
2024-10-01 18:28:44 +0530

    view details button add

--- src/components/common/MovieCard.jsx
+++ src/components/common/MovieCard.jsx
@@ -11,16 +11,18 @@ const MovieCard = ({ item, onAddToWishlist }) => {
       transition={{ delay: item * 0.09, duration: 1 }}
       exit={{ scale: 0, opacity: 0, y: 20 }}
     >
-      {" "}
-      <div className="!h-64 relative !w-11/12 rounded-xl cursor-pointer !overflow-hidden">
+      <div className="!h-64 relative !w-11/12 group rounded-xl cursor-pointer !overflow-hidden">
         <img
           src={
             item.Poster !== "N/A"
               ? item.Poster
               : "https://lumiere-a.akamaihd.net/v1/images/p_encanto_homeent_22359_4892ae1c.jpeg" // Default image
           }
-          className="object-cover h-full w-full"
+          className="object-cover h-full w-full transition-transform duration-300 group-hover:scale-110"
         />
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+          <h2 className="text-white font-semibold text-base">View Details</h2>
+        </div>
         <div
           onClick={() => onAddToWishlist(item)}
           className="bg-primary hover:bg-white hover:text-primary  absolute rounded-full top-3 right-3 text-white h-8 w-8 flex items-center justify-center  "
